698e1a44e9338e202c806616be88bb1d63476f06
Jonas Neichin <jonas.neichin@wustl.edu>
2017-07-06 14:36:49 -0700

    extend base

--- app/views.py
+++ app/views.py
@@ -2,8 +2,6 @@ from flask import render_template
 from app import app
 
 @app.route('/') 
-def base():
-	return render_template('base.html', title="Jonas' Blog")
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
