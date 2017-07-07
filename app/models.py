1ec77683a8b1f95a9be5354da2f6050829daf225
Jonas Neichin <jonas.neichin@wustl.edu>
2017-07-07 10:22:19 -0700

    timestamp issue

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	datetime = db.Column(db.Timestamp)
+	datetime = db.Column(db.TIMESTAMP)
 	title = db.Column(db.String(120), index=True)
 	content= db.Column(db.Text)
 
