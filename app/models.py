2c6429ccfd6e80ed02a7f73d0898c0cce5e9cd3b
Jonas Neichin <jonas.neichin@wustl.edu>
2017-07-07 10:16:50 -0700

    now actually added db files

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	datetime = db.Column(db.Timestamp)
+	title = db.Column(db.String(120), index=True)
+	content= db.Column(db.Text)
+
+	def __repr__(self):
+		return '<Post %r>' % (self.id)
