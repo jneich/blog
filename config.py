127743c37b3153345644ace0b06b10fbaff0aed9
Jonas Neichin <jonas.neichin@wustl.edu>
2017-07-07 10:15:49 -0700

    add db create and migrate files

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '43qb6e(rl$+=-jbk_j+*@4*g=)#ss$rxswo14pi2r&l)f+uj98jtn'
+
+
 
